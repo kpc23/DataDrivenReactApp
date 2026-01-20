9acd33ffb97776d284da16b655f92832976815da
Karla Carrillo <karla.carrillo@centre.edu>
2026-01-20 15:41:06 -0500

    Part one of table header. Step 4 done. TODO: step: 5 useState() /step 6: small table.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from "react";
 
 function App() {
+	const [data, setData] = useState([{}]);
+
+	useEffect(() => 
+  {
+		async function fetchData()
+    {
+			const url = "/NEH2020sGrant_Short.json";
+			const response = await fetch(url);
+
+      if (response.ok) 
+      {
+		    const result = await response.json();
+        console.log(result.Grants.Grant);
+
+        setData(result.data);
+      }
+		}
+		fetchData();
+
+	}, [data]);
+  
+// function TableRow(props){
+// 
+// }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +45,31 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+
+      </header> */}
+      
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Grant Project
+            </th>
+            <td>
+               - Institution
+            </td>
+          </tr>
+        </thead>
+        
+        <tbody> 
+          {/*Grant information Here */}
+
+          
+
+        </tbody>
+      </table>
+
+
+
     </div>
   );
 }
