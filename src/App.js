e1a7b510ecd7f74d6c4a9c804c9c9235a8b85a51
Karla Carrillo <karla.carrillo@centre.edu>
2026-01-20 20:34:39 -0500

    Created second useState, working on attempting to add 5 different interactive components...

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { useEffect, useState} from "react";
 
 function App() {
 	const [data, setData] = useState([]);
-
+  /*New consts useState for filtering and sorting*/
+  const [updatedData, setUpdatedData] = useState(data);
 	useEffect(() => 
   {
 		async function fetchData()
@@ -16,74 +17,92 @@ function App() {
       if (response.ok) 
       {
 		    const result = await response.json();
-        // console.log(result.Grants.Grant);
-
-        setData(result.Grants.Grant);
+        let finalresult=result.Grants.Grant;
+        setData(finalresult);
+        setUpdatedData(finalresult)
       }
 		}
 		fetchData();
 
 	}, []);
+
+  // const restoreData = (data) =>
+
+
   
-function TableRow(grant){
-  return (
-    <tr>
-      <td>{grant.grant.InstCity}</td>
-      <td>{grant.grant.Division}</td>
-      <td>{grant.grant.ProjectTitle}</td>
-      <td>{grant.grant.Institution}</td>
-      <td>{grant.grant.PrimaryDiscipline}</td>
-      <td>{grant.grant.ProjectDesc}</td>
-      <td>{grant.grant.YearAwarded}</td>
-    </tr>
-  );
-}
+  function TableRow(grant){
+    return (
+      <tr>
+        <td title = {grant.grant.InstState}>{grant.grant.InstCity}</td>
+        <td>{grant.grant.Division}</td>
+        <td>{grant.grant.ProjectTitle}</td>
+        <td>{grant.grant.Institution}</td>
+        <td>{grant.grant.PrimaryDiscipline}</td>
+        <td>{grant.grant.Program}</td>
+        <td>{grant.grant.YearAwarded}</td>
+      </tr>
+    );
+  }
 
-function Table({grants}){
-  return(
-    <table border= {1}>
-      <thead>
-          <tr>
-            <th>
-              City
-            </th>
-            <th>
-              Division
-            </th>
-            <th>
-              Grant Project
-            </th>
-            <th>
-              Institution
-            </th>
-            <th>
-              Primary Discipline
-            </th>
-            <th>
-              Project Description
-            </th>
-            <th>
-              Year Awarded
-            </th>
-          </tr>
-        </thead>
-        
-        <tbody> 
-          {grants.map((grant, index) =>
+  function Table({grants}){
+    return(
+      <table border= {1}>
+        <caption>
+          <h1>
+          Grant Information
+          </h1>
+        </caption>
+
+        <thead>
+            <tr>
+              <th>
+                City
+              </th>
+              <th>
+                Division
+              </th>
+              <th>
+                Grant Project
+              </th>
+              <th>
+                Institution
+              </th>
+              <th>
+                Primary Discipline
+              </th>
+              <th>
+                Program
+              </th>
+              <th>
+                Year Awarded
+              </th>
+            </tr>
+          </thead>
           
-            <TableRow key = {index}
-                      grant = {grant}
-            />
-          )}
+          <tbody> 
+            {grants.map((grant, index) =>
+            
+              <TableRow key = {index}
+                        grant = {grant}
+              />
+            )}
 
-        </tbody>
-    </table>
+          </tbody>
+      </table>
 
-  )
-}
+    )
+  }
 
   return (
     <div className="App">
+      {/* <label>	useEffect(() => 
+  {
+
+        Filter by Institution:
+        <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)}
+        placeholder='Name of Institution'
+        />
+      </label> */}
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -99,7 +118,8 @@ function Table({grants}){
         </a>
 
       </header> */}
-      <Table grants = {data}/>
+      {/* <Table grants = {data}/> */}
+      <Table grants = {updatedData}/>
     </div>
   );
 }
